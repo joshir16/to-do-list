ae00c194883f340c3672e770975b716850063577
Rishabh Joshi <joshir838@gmail.com>
2021-12-02 19:22:38 +0530

    new to-do

--- js/script.js
+++ js/script.js
@@ -1,57 +1,63 @@
-const inputText = document.querySelector(".input__item");
-const addBtn = document.querySelector(".add__btn");
+const input = document.getElementById("input");
+const addBtn = document.getElementById("add__btn");
+const list = document.getElementById("list");
 
-// .............................................................
-const list = document.querySelector(".list");
+const items = document.querySelectorAll(".items");
+const noOfTasks = document.querySelector(".task-num");
 
-// .............................................................
-const allitems = document.querySelectorAll(".item");
-
-// .............................................................
-const deleteBtn = document.querySelector(".delete__item");
-
-// ========================================================
+// =================================================================
 let itemNum = 0;
-// ========================================================
-const addItem = function () {
-  const itemText = inputText.value;
+noOfTasks.innerText = 0;
+// =================================================================
 
-  if (itemText.trim().length > 0) {
+const addItem = function () {
+  const inputText = input.value;
+
+  if (inputText.trim().length > 0) {
     const item = `
-    <li class="home__box__item item">
-      <input type="checkbox"  id="item--${itemNum}" class="item__checkbox" />
-      <label for="item--${itemNum}" class="item__checkbox__label">${itemText}</label>
-        <span class="material-icons delete__item"> clear </span>
-    </li>`;
+    <li class="home__item item">
+        <div class="home__item__text">
+            <input type="checkbox" id="checkbox-${itemNum}"
+                class="home__item__text__checkbox" />
+            <span class="home__item__text__checkbox__span"></span>
+            <label for="checkbox-${itemNum}" class="home__item__text__task"> 
+                ${inputText}
+            </label>
+        </div>
+        <div class="home__item__icons">
+            <span class="material-icons home__item__icons__delete delete__btn">
+                clear
+            </span>
+            </div>
+    </li>  `;
 
     itemNum++;
+    noOfTasks.innerText++;
 
     list.insertAdjacentHTML("beforeend", item);
+
+    input.value = "";
+    input.focus();
   }
 };
 
-addBtn.addEventListener("click", function () {
-  addItem();
-  inputText.value = "";
-  inputText.focus();
-});
+// =================================================================
+
+addBtn.addEventListener("click", addItem);
 
 document.addEventListener("keydown", function (e) {
   if (e.key === "Enter") {
     addItem();
-    inputText.value = "";
-    inputText.focus();
   }
 });
 
-inputText.focus();
-
-// ==========================================================
-// delete li -----------------------------------------------
+// =================================================================
+// =================================================================
 list.addEventListener("click", function (e) {
   let closestLi = e.target.closest(".item");
 
-  if (e.target.classList.contains("delete__item")) {
+  if (e.target.classList.contains("delete__btn")) {
     closestLi.parentNode.removeChild(closestLi);
+    noOfTasks.innerText--;
   }
 });
